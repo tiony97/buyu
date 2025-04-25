722c94659cad6327ae37c7c4749ab9a379a0d679
tiony97 <kipanthony97@gmail.com>
2025-04-25 12:45:50 +0300

    completed menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,27 @@ $(document).ready(() => {
   let animation = gsap.timeline({ paused: true, reversed: true });
   animation
     .to("header", { opacity: 0, duration: 0.5, ease: "power2.out" })
-
-    .to(".overlay-menu", { height: "100vh", duration: 0.5, ease: "power2.in" })
+    .to(
+      ".center-piece",
+      { opacity: 0, duration: 0.5, ease: "power2.out" },
+      "-=0.2"
+    )
+    .to(
+      ".overlay-menu",
+      {
+        opacity: 1,
+        visibility: "visible",
+        pointerEvents: "all",
+        duration: 0.6,
+        ease: "power2.in",
+      },
+      "-=0.1"
+    )
 
     .to(".overlay-links a", {
       opacity: 1,
       duration: 0.5,
-      stagger: { each: 0.1, ease: "power2.in" },
+      stagger: { each: 0.1, ease: "power3.in" },
     });
 
   $("#menuOpen").click(() => {
